86f09ca0f98f8c23c2f468ed4f425a49bc3575ec
Ali <90100667+zeqley@users.noreply.github.com>
2023-04-25 23:16:23 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,21 +1,14 @@
-
-    if (localStorage.getItem = ('status') == null) {
-        location.href = "./index.html";
-    }
-
-
 function login() {
     let username = document.getElementById("username").value;
     let password = document.getElementById("password").value; 
     if (username == "admin" && password == "admin") {
         //alert("Giriş Başarılı");
         location.href = "./main.html";
-        preventDefault();
-        localStorage.setItem('status', 'PASS');
+        sessionStorage.setItem("loggedIn", "true");
+
     }
             else {
         alert("Giriş Başarısız");
 
     }
         }
-
